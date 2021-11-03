b9e0c30ca7619c7055b850662b1fff58b9995fe7
AbhimanyuRana <91766052+AbhumanyuXMusic@users.noreply.github.com>
2021-11-03 18:08:25 +0530

    Update downloader.py

--- asadmodules/downloader.py
+++ asadmodules/downloader.py
@@ -64,7 +64,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎧 Uᴘʟᴏᴀᴅᴇʀ @Venom_Hai_Hum**"
+        rep = f"**🎧 Uᴘʟᴏᴀᴅᴇʀ @Itz_VeNom_xD**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
